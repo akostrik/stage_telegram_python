9628637c93d6fac40aba01a48eb0a92f202d0e60
ann <internet124816@gmail.com>
2023-10-06 23:32:01 +0200

    -

---
+++ frontend/src/components/BinaryEditor.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <h1>Edit Binary Affirmation for ID: {{ affirmation._id }}</h1>
+
+    <div>
+      <label for="messageText">Message:</label>
+      <textarea id="messageText" v-model="affirmation.text"></textarea>
+    </div>
+
+    <h2>GPT Extracted Affirmations:</h2>
+    <table>
+      <tbody>
+        <tr v-for="(value, key, index) in affirmation.affirmations" :key="'gpt-' + index">
+          <td><textarea v-model="affirmationKeys.gpt[index]" /></td>
+          <td><textarea v-model="affirmation.affirmations[key]" /></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h2>Corrected Affirmations:</h2>
+    <table>
+      <tbody>
+        <tr v-for="(value, key, index) in affirmation.affirmations_corrections" :key="'corrected-' + index">
+          <td><textarea v-model="affirmationKeys.corrected[index]" /></td>
+          <td><textarea v-model="affirmation.affirmations_corrections[key]" /></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div>
+      <input type="checkbox" v-model="affirmation.to_use_a"> Use this affirmation
+    </div>
+
+    <button @click="saveChanges">Save</button>
+    <button @click="$emit('backToLanding')">Back</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    affirmation: Object
+  },
+  data() {
+    return {
+      affirmationKeys: {
+        gpt: Object.keys(this.affirmation.affirmations),
+        corrected: Object.keys(this.affirmation.affirmations_corrections)
+      }
+    }
+  },
+  watch: {
+    affirmation: {
+      deep: true,
+      handler(newAffirmation) {
+        this.affirmationKeys.gpt = Object.keys(newAffirmation.affirmations);
+        this.affirmationKeys.corrected = Object.keys(newAffirmation.affirmations_corrections);
+      }
+    }
+  },
+  methods: {
+  saveChanges() {
+    // Convert arrays back to objects based on the modified keys
+    this.affirmation.affirmations = this.affirmationKeys.gpt.reduce((acc, key, index) => {
+      if (key.trim() !== "") {
+        acc[key] = Object.values(this.affirmation.affirmations)[index];
+      }
+      return acc;
+    }, {});
+    this.affirmation.affirmations_corrections = this.affirmationKeys.corrected.reduce((acc, key, index) => {
+      if (key.trim() !== "") {
+        acc[key] = Object.values(this.affirmation.affirmations_corrections)[index];
+      }
+      return acc;
+    }, {});
+
+    const updateData = { ...this.affirmation };
+    delete updateData._id;
+
+    fetch(`http://localhost:3001/update/${this.affirmation._id}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updateData)
+    })
+    .then(res => res.json())
+    .then(response => {
+      if (response.success) {
+        alert('Affirmation saved successfully!');
+        this.$emit('saved');
+      } else {
+        alert('Error saving affirmation. Please try again.');
+      }
+    });
+  }
+}
+
+  
+};
+</script>
---
+++ frontend/src/components/DataEditor.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <h1>Edit Data for ID: {{ record._id }}</h1>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Characteristics</th>
+          <th>Score</th>
+          <th>Corrected Score</th>
+          <th>Explanation</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(char, index) in characteristics" :key="char._id">
+          <td>{{ char.str }}</td>
+          <td><input type="number" v-model="record.score_list[index]" /></td>
+          <td><input type="number" v-model="record.score_list_corrections[index]" /></td>
+          <td><input v-model="record.score_list_explanations[char.str]" /></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h2>Usage:</h2>
+    <input type="checkbox" v-model="record.to_use_c"> Use this record
+
+    <br><br>
+    <button @click="saveChanges">Save</button>
+    <button @click="$emit('cancel')">Cancel</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    record: Object
+  },
+  data() {
+    return {
+      characteristics: []
+    };
+  },
+  methods: {
+    saveChanges() {
+        const updateData = { ...this.record };
+        delete updateData._id; // Remove the _id field from the data to be sent
+
+        fetch(`http://localhost:3001/update/${this.record._id}`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updateData)
+        })
+        .then(res => res.json())
+        .then(response => {
+            if (response.success) {
+            alert('Data saved successfully!');
+            this.$emit('saved'); // Notify the parent component
+            } else {
+            alert('Error saving data. Please try again.');
+            }
+        });
+    }
+
+  },
+  mounted() {
+    fetch('http://localhost:3001/characteristics')
+      .then(response => response.json())
+      .then(data => {
+        this.characteristics = data;
+        console.log(data)
+      });
+  },
+};
+</script>
+
+<style scoped>
+/* You can add styles specific to this component here if needed */
+</style>
