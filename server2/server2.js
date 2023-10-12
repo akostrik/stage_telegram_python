70352135a61af43d781522510e3163ecd1d50f9a
akostrik <internet124816@gmail.com>
2023-10-13 01:34:40 +0200

    node.js

--- server2/server2.js
+++ server2/server2.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express     = require('express');
+const cors        = require('cors');
+const bodyParser  = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId    = require('mongodb').ObjectId;
 
-const app = express();
-const port = 3001;
+const app         = express();
+const port        = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const mongoUrl = 'mongodb+srv://anna:1AQ2ZS3ED@cluster0.pysxbay.mongodb.net/?retryWrites=true&w=majority';
-const dbName = 'telegram';
+const mongoUrl    = 'mongodb+srv://anna:1AQ2ZS3ED@cluster0.pysxbay.mongodb.net/?retryWrites=true&w=majority';
+const dbName      = 'telegram';
 
 let db;
 
@@ -25,6 +25,36 @@ const connectToDB = async () => {
   });
 }
 
+app.post('/saveChannel', async (req, res) => {
+  try {
+    let { channelId } = req.body;
+    channelId = parseInt(channelId, 10);
+    if (isNaN(channelId)) {
+      throw new Error('Invalid channelId');
+    }
+    await db.collection('channels').insertOne({ channelId });
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+});
+
+app.get('/channels', async (req, res) => {
+  try {
+    const docs = await db.collection('channels').find({}).limit(100).toArray();
+    res.json(docs);
+  } catch (error) {
+    res.status(500).send('Server Error');
+  }
+});
+
+app.get('/relations', async (req, res) => {
+  const docs = await db.collection('relations').find({}).limit(100).toArray();
+  res.json(docs);
+});
+
+
+// Learning :
 app.get('/message/:id', async (req, res) => {
   const id = req.params.id;
   const doc = await db.collection('messages').findOne({ _id: new ObjectId(id) });
@@ -41,34 +71,12 @@ app.get('/characteristics', async (req, res) => {
   res.json(docs);
 });
 
-app.get('/relations', async (req, res) => {
-  const docs = await db.collection('channels_similarity').find({}).limit(100).toArray();
-  res.json(docs);
-});
-
 app.post('/update/:id', async (req, res) => {
   const id = req.params.id;
   await db.collection('messages').updateOne({ _id: new ObjectId(id) }, { $set: req.body });
   res.json({ success: true });
 });
 
-app.post('/saveChannel', async (req, res) => {
-  try {
-    let { channelId } = req.body;
-
-    // Convert channelId to integer, if it's not already
-    channelId = parseInt(channelId, 10);
-
-    if (isNaN(channelId)) {
-      throw new Error('Invalid channelId');
-    }
-
-    await db.collection('channels_id').insertOne({ channelId });
-    res.json({ success: true });
-  } catch (error) {
-    res.json({ success: false, message: error.message });
-  }
-});
 
 // Centralized error handling
 app.use((err, req, res, next) => {
@@ -77,38 +85,3 @@ app.use((err, req, res, next) => {
 });
 
 connectToDB().catch(err => console.error('Failed to connect to the database', err));
-
-/*app.get('/data/:id', async (req, res) => {
-  const id = req.params.id;
-  const doc = await db.collection('channels_scores').findOne({ _id: new ObjectId(id) });
-  res.json(doc);
-});
-
-app.get('/all_records', async (req, res) => {
-  const docs = await db.collection('channels_scores').find({}).limit(100).toArray();
-  res.json(docs);
-});
-
-app.post('/update/:id', async (req, res) => {
-  const id = req.params.id;
-  await db.collection('channels_scores').updateOne({ _id: new ObjectId(id) }, { $set: req.body });
-  res.json({ success: true });
-});
-
-app.get('/all_affirmations', async (req, res) => {
-  const docs = await db.collection('affirmations').find({}).limit(100).toArray();
-  res.json(docs);
-});
-
-app.get('/affirmation/:id', async (req, res) => {
-  const id = req.params.id;
-  const doc = await db.collection('affirmations').findOne({ _id: new ObjectId(id) });
-  res.json(doc);
-});
-
-app.post('/update_affirmation/:id', async (req, res) => {
-  const id = req.params.id;
-  await db.collection('affirmations').updateOne({ _id: new ObjectId(id) }, { $set: req.body });
-  res.json({ success: true });
-});*/
-
